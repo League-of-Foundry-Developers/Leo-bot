2fc03487144da8824576d1e930525bc3bc9b3b5c
zeel <zeel02@gmail.com>
2021-03-31 01:17:03 -0400

    Added Foundry Hub search, stats, and info

--- main.js
+++ main.js
@@ -21,19 +21,34 @@ async function getPackageResponse(name) {
 	const pkg = await getPackage(name);
 	const package = pkg.package;
 	const manifest = pkg.manifest;
+	const fhub = pkg.fhub;
 
-	if (!package) return { content: `Package \`${name}\` not found!` };
-
-	return {
+	return package ? {
 		content: `Package: \`${name}\``,
-		embeds: [getPackageEmbed(package, manifest)]
+		embeds: [getPackageEmbed(package, manifest, fhub)]
+	} : {
+		content: `Package \`${name}\` not found!`,
+		embeds: [await getPackageSearchEmbed(name)]
 	}
 }
 
-function getPackageEmbed(package, manifest) {
+async function getPackageSearchEmbed(query) {
+	const search = await searchPackage(query);
+	const options = search.map(n => "`" + n + "`").join("\n");
+
+	return {
+		color: 0xff6400,
+		title: "Similar Packages",
+		description: options
+	}
+
+}
+
+function getPackageEmbed(package, manifest, fhub) {
 	const image = package.media.find(m => m.type == "cover")?.url
 	const thumb = package.media.find(m => m.type == "icon")?.url
 	const author = package.authors.join(", ");
+	const system = manifest.systems?.join(", ");
 
 	const embed = { 
 		color: 0xff6400,
@@ -50,13 +65,33 @@ function getPackageEmbed(package, manifest) {
 		},
 		fields: [
 			{
+				name: "Info:",
+				value: `
+**Version:** ${manifest.version}
+**Core:** ${manifest.compatibleCoreVersion}
+${system ? `**System:** \`${system}\`` : ""}
+${manifest.changelog ? `[Changelog](${manifest.changelog})` : ""}
+					`,
+				inline: true
+			},
+			{
+				name: "Stats:",
+				value: `
+**Installs:** ${package.installs}%
+**Likes:** ${fhub.endorsements}
+**Comments:** ${fhub.comments}
+					`,
+				inline: true
+			},
+			{
 				name: "Links:",
-				value: `[Project Website](${package.url})
-						[Manifest](${manifest.manifest})
-						[Package Listing](https://foundryvtt.com/packages/${package.name})
-						[Foundry Hub](https://www.foundryvtt-hub.com/package/${package.name})
-						[The Forge Bazaar](https://forge-vtt.com/bazaar#package=${package.name})
-						`,
+				value: `
+**[Manifest](${manifest.manifest})**
+[Project Website](${package.url})
+[Package Listing](https://foundryvtt.com/packages/${package.name})
+[Foundry Hub](https://www.foundryvtt-hub.com/package/${package.name})
+[The Forge Bazaar](https://forge-vtt.com/bazaar#package=${package.name})
+`,
 				inline: true
 			}
 		]
@@ -75,16 +110,33 @@ function getPackageEmbed(package, manifest) {
 	return embed;
 }
 
+async function searchPackage(query) {
+	const response = await fetch(`https://www.foundryvtt-hub.com/wp-json/relevanssi/v1/search?posts_per_page=5&paged=1&type=package&keyword=${query}`);
+	let data = await response.json();
+	if (!data || data?.code == "No results") data = [];
+
+	return data.map(p => p.slug);
+}
 async function getPackage(name) {
-	const response = await fetch(`https://forge-vtt.com/api/bazaar/package/${name}?manifest=1`);
-	return await response.json();
+	const hub = fetch(`https://www.foundryvtt-hub.com/wp-json/hubapi/v1/package/${name}`)
+		.then(r => r.json());
+	const bazaar = fetch(`https://forge-vtt.com/api/bazaar/package/${name}?manifest=1`)
+		.then(r => r.json());
+
+	const [hubResp, bazResp] = await Promise.all([hub, bazaar]);
+
+	return {
+		package: bazResp.package,
+		manifest: bazResp.manifest,
+		fhub: hubResp
+	}
 }
 
 client.ws.on('INTERACTION_CREATE', async interaction => {
 	//console.log(interaction);
 	console.log(interaction.data.options);
 
-	const name = interaction.data.options.find(o => o.name == "package-name")?.value;
+	const name = interaction.data.options.find(o => o.name == "name")?.value;
 	console.log(`Package Name: ${name}`);
 
 	client.api.interactions(interaction.id, interaction.token).callback.post({data: {
