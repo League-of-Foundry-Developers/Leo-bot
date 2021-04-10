89312df303f0a1ea550c1877fb3d9b793ffea622
zeel <zeel02@gmail.com>
2021-04-10 02:59:07 -0400

    Added InteractionResposne definition

--- classes/typedefs.js
+++ classes/typedefs.js
@@ -31,33 +31,44 @@
  * An interaction is the base "thing" that is sent when a user invokes
  * a command, and is the same for Slash Commands and other future interaction types.
  *
- * @typedef {object} Interaction
- * @property {string} id              - id of the interaction
- * @property {string} application_id  - id of the application this interaction is for
- * @property {number} type            - the type of interaction
- * @property {InteractionData} [data] - the command data payload
- * @property {string} [guuild_id]     - the guild it was sent from
- * @property {string} [channel_id]    - the channel it was sent from
- * @property {object} [member]        - guild member data for the invoking user, including permissions
- * @property {object} [user]          - user object for the invoking user, if invoked in a DM
- * @property {string} token           - a continuation token for responding to the interaction
- * @property {number} version         - read-only property, always 1
+ * @typedef  {object} Interaction
+ * @property {string}          id              - id of the interaction
+ * @property {string}          application_id  - id of the application this interaction is for
+ * @property {number}          type            - the type of interaction
+ * @property {InteractionData} [data]          - the command data payload
+ * @property {string}          [guuild_id]     - the guild it was sent from
+ * @property {string}          [channel_id]    - the channel it was sent from
+ * @property {object}          [member]        - guild member data for the invoking user, including permissions
+ * @property {object}          [user]          - user object for the invoking user, if invoked in a DM
+ * @property {string}          token           - a continuation token for responding to the interaction
+ * @property {number}          version         - read-only property, always 1
  */
 
 /**
  * @typedef {ApplicationCommandInteractionData} InteractionData
  *//**
- * @typedef {object} ApplicationCommandInteractionData
- * @property {string} id                          - the ID of the invoked command
- * @property {string} name                        - the name of the invoked command
+ * @typedef  {object} ApplicationCommandInteractionData
+ * @property {string}                   id        - the ID of the invoked command
+ * @property {string}                   name      - the name of the invoked command
  * @property {Array<InteractionOption>} [options] - the params + values from the user
  */
 
- /**
+/**
  * @typedef {ApplicationCommandInteractionDataOption} InteractionOption
  *//**
- * @typedef {object} ApplicationCommandInteractionDataOption
- * @property {string} name                        - the name of the parameter
- * @property {string} [value]                     - the value of the pair
+ * @typedef  {object} ApplicationCommandInteractionDataOption
+ * @property {string}                   name      - the name of the parameter
+ * @property {string}                   [value]   - the value of the pair
  * @property {Array<InteractionOption>} [options] - present if this option is a group or subcommand
+ */
+
+/** 
+ * @typedef {InteractionApplicationCommandCallbackData} InteractionResposne
+ *//**
+ * @typedef  {object} InteractionApplicationCommandCallbackData
+ * @property {boolean}                                  [tts]              - is the response TTS
+ * @property {string}                                   [content]          - message content
+ * @property {Array<import("discord.js").MessageEmbed>} [embeds]           - supports up to 10 embeds
+ * @property {object}                                   [allowed_mentions] - allowed mentions object
+ * @property {number}                                   [flags]            - set to 64 to make your response ephemeral
  */
