bc32809e3712f8ac1df20c6daa4d22363cf5cfd8
zeel <zeel02@gmail.com>
2021-04-12 00:27:29 -0400

    Improved main file

--- main.js
+++ main.js
@@ -9,20 +9,26 @@ import Leo from "./Leo.js";
 const { Client, Intents } = discord;
 const { Sequelize, DataTypes, Model } = seq;
 
-
-const sequelize = new Sequelize({
-	dialect: "sqlite",
-	storage: config.database,
-	logging: utils.debug
-});
-
-const client = new Client({
-	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-	intents: Intents.NON_PRIVILEGED
-});
-
+/**
+ * The primary function that sets everythign into motion.
+ * Creates instances of the SQL ORM, and Discord.js client,
+ * then creates an instance of Leo with them, and initializes it.
+ */
 async function main() {
+	const sequelize = new Sequelize({
+		dialect: "sqlite",        // Leo uses an sqlite database
+		storage: config.database, // The .db location is defined in the config.json file
+		logging: utils.debug      // Send db query logs through an output wrapper
+	});
+
+	const client = new Client({
+		partials: ['MESSAGE', 'CHANNEL', 'REACTION'], // Get partial data structures for these
+		intents: Intents.NON_PRIVILEGED               // Leo wants to do anything that isn't "privileged"
+	});
+
+	// Roar! 🦁
 	const leo = new Leo(config, sequelize, client);
+
 	await leo.init();
 }
 
