9e047214d6513fd065e8e34532a56ec7e0198f95
zeel <zeel02@gmail.com>
2021-04-11 21:06:23 -0400

    Added docs

--- Leo.js
+++ Leo.js
@@ -1,9 +1,11 @@
-const { Client } = require("discord.js");
 const { PackageSearch } = require("./classes/PackageSearch.js");
 const { ReputationManager } = require("./classes/ReputationManager.js");
 
 /**
  * @typedef {import("discord.js").Client} Client
+ * @typedef {import("discord.js").Message} Message
+ * @typedef {import("discord.js").MessageReaction} MessageReaction
+ * @typedef {import("discord.js").User} User
  * @typedef {import("sequelize").Sequelize} Sequelize
  */
 
@@ -29,7 +31,12 @@ class Leo {
 		this.reputation = new ReputationManager(this);
 		this.packages   = new PackageSearch(this);
 	}
-
+	
+	/**
+	 * Initialize required members and structures for Leo
+	 *
+	 * @memberof Leo
+	 */
 	async init() {
 		await this.reputation.init();
 		await this.packages.init();
@@ -37,11 +44,26 @@ class Leo {
 		await this.client.login(this.config.token);
 	}
 
-	debug(...args) {
+	/**
+	 * Sends a message to the console only if debug mode is on.
+	 *
+	 * @param {Array} args - Any arguments, which are passed directly to console.debug
+	 * @return {void}        Returns early if debugging is disabled 
+	 * @memberof Leo
+	 */
+	static debug(...args) {
 		if (!this.config.debug) return;
 		console.debug(...args);
 	}
 
+	/**
+	 * Wraps fetching the remaining data for a partial Discord.js structure
+	 * in a condition and error handling.
+	 *
+	 * @param {object} data - Some Discord.js object
+	 * @return {boolean}      True if the partial was fetches successfully
+	 * @memberof Leo
+	 */
 	async fetchPartial(data) {
 		if (data.partial) {
 			try { await data.fetch(); } catch (error) {
@@ -52,12 +74,29 @@ class Leo {
 		return true;
 	}
 
+	/**
+	 * Fires once when the bot is ready.
+	 *
+	 * @memberof Leo
+	 */
 	async onReady() {
 		console.log("Leo ready!");
 	}
+
+	/**
+	 * Receives message creation events, and delegates handling them.
+	 *
+	 * Ignores bots.
+	 *
+	 * Eats errors.
+	 *
+	 * @param {Message} message
+	 * @return {void} 
+	 * @memberof Leo
+	 */
 	async onMessage(message) {
 		if (message.author.bot) return;
-		this.debug(message);
+		Leo.debug(message);
 		
 		try {
 			await Promise.all([
@@ -65,8 +104,20 @@ class Leo {
 			]);
 		} catch (e) { console.error(e); }
 	}
+
+	/**
+	 * Receives message reaction creation events, and delegates handling them.
+	 *
+	 * Eats errors.
+	 *
+	 * @param {MessageReaction} reaction - The reaction that was created
+	 * @param {User}            user     - The user who reacted
+	 * @return {void} 
+	 * @memberof Leo
+	 */
 	async onMessageReactionAdd(reaction, user) {
 		if (!await this.fetchPartial(reaction)) return;
+		Leo.debug(reaction, user);
 
 		try{
 			await Promise.all([
@@ -82,6 +133,8 @@ class Leo {
 	 * @memberof Leo
 	 */
 	async onInteractionCreate(interaction) {
+		Leo.debug(interaction);
+
 		try {
 			await Promise.all([
 				this.reputation.handleInteraction(interaction),
@@ -90,6 +143,11 @@ class Leo {
 		} catch (e) { console.error(e); }
 	}
 
+	/**
+	 * Registers the event handlers for this bot
+	 *
+	 * @memberof Leo
+	 */
 	async createListeners() {
 		this.client.once("ready", this.onReady.bind(this));
 		this.client.on("message", this.onMessage.bind(this));
@@ -98,6 +156,13 @@ class Leo {
 		this.client.ws.on('INTERACTION_CREATE', this.onInteractionCreate.bind(this));
 	}
 
+	/**
+	 * Sends a response to an interaction
+	 *
+	 * @param {Interaction} interaction
+	 * @param {InteractionResponse} data
+	 * @memberof Leo
+	 */
 	async respond(interaction, data) {
 		return await this.client.api
 			.interactions(interaction.id, interaction.token)
@@ -108,6 +173,9 @@ class Leo {
 module.exports.Leo = Leo;
 
 /* Leftovers from earlier development not yet re-factored
+
+// A command to make Leo send a message in a certain channel,
+// Useless without better permissions system
 async function handleSayCommand(interaction) {
 	console.log(client.channels);
 	const channel = interaction.data.options.find(o => o.name == "channel")?.value;
