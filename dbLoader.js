719b6c2ea2f8a31875aa40a020315752a0e0a077
zeel <zeel02@gmail.com>
2021-04-13 10:32:58 -0400

    Added loading of user names

--- dbLoader.js
+++ dbLoader.js
@@ -1,14 +1,15 @@
 /**
  * Call this file like:
- * `node dbLoader.js ./config.json ./yag-export.json`
+ * `node dbLoader.js ./test.config.json ./yag-export.json`
  * Where the first argument is the config file for your Leo,
  * and the second is data exported from YAGPDB.xyz
  */
 
 import config from "./config.cjs";
-import { Reputation } from "./database.js";
+import * as Database from "./database.js";
 import seq from 'sequelize';
 import fs from "fs/promises";
+import { User } from "discord.js";
 
 const { Sequelize } = seq;
 
@@ -18,9 +19,11 @@ const sequelize = new Sequelize({
 });
 
 async function main() {
-	Reputation.init(sequelize);
+	Database.Reputation.init(sequelize);
+	Database.User.init(sequelize);
 
-	await Reputation.sync({ alter: true });
+	await Database.Reputation.sync({ alter: true });
+	await Database.User.sync({ alter: true });
 
 	const data = JSON.parse(await fs.readFile(process.argv[3]));
 
@@ -36,7 +39,25 @@ async function insertData(data) {
 		createdAt: d.created_at
 	}));
 
-	await Reputation.bulkCreate(toInsert);
+	const users = data
+		.filter(d => d.receiver_username)
+		.map(d => {
+			const [tag, name, disc] = d.receiver_username.match(/(.+)#([0-9]+)/);
+			return {
+				user: d.receiver_id,
+				name: name,
+				discriminator: disc
+			}
+		});
+
+	
+	await Database.Reputation.bulkCreate(toInsert);
+	
+	
+	for (let user of users) {
+		await Database.User.upsert(user);
+	}
+	console.log(users);
 }
 
 main();
