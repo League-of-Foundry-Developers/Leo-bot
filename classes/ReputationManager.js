d6b9c80d2413ba894ae40d970acf0e6a7eb606ab
arcanist <82790112+arcanistzed@users.noreply.github.com>
2021-05-23 13:32:26 -0400

    Don't give points to people saying "no thanks"

--- classes/ReputationManager.js
+++ classes/ReputationManager.js
@@ -111,12 +111,13 @@ export default class ReputationManager extends InteractionHandler {
 	 * Determin if a message contains one of the triggers for giving reputation.
 	 *
 	 * @param {Message} message - A message to test
+	 
 	 * @return {boolean}          True if the message contains one of the triggers
 	 * @memberof ReputationManager
 	 */
 	_testMessage(message) {
 		return [
-			m => /(?<![A-z])thanks?(?![A-z])/gi.test(m.content),               // Contains "thank"
+			m => /(?<!no )(?<![A-z])thanks?(?![A-z])/gi.test(m.content),       // Contains "thank", but not preceeded by "no"
 			m => /(?<![A-z])tyvm(?![A-z])/gi.test(m.content),                  // Constains "tyvm"
 			m => /(?<![A-z])points? (?:to|for) <@(?![A-z])/gi.test(m.content), // Phrase like "a point to [user]" - think Harry Potter
 			m => /:vote:/gi.test(m.content)                                    // The +1 emoji
